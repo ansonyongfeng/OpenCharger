d8d98edf325577cab07587bc5c4cee29b9e299e7
ansonyongfeng <heyongfeng@gmail.com>
2014-04-30 17:10:37 +0200

    Demo App
    kick off

---
+++ OpenChargerDemo/OpenChargerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenChargerDemo
+//
+//  Created by YongfengHe on 30.04.14.
+//  Copyright (c) 2014 Yongfeng He. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
